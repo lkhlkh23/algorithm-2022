030168780756eafaed7654c123814303e05e7ae2
프로모션플랫폼개발팀 이기현 <lkhlkh23@sk.com>
2022-05-15 22:34:59 +0900

    programmers | Q-87389

---
+++ src/main/java/programmers/Q87389/Solution.java
@@ -0,0 +1,14 @@
+package programmers.Q87389;
+
+class Solution {
+
+    public static int solution(final int n) {
+        for (int i = 2; i < n; i++) {
+            if(n % i == 1) {
+                return i;
+            }
+        }
+        return 2;
+    }
+
+}
