da8df728d468dad4a18d7d55b0e0987d32ad5759
lkhlkh23 <lkhlkh09@gmail.com>
2022-06-21 00:01:06 +0900

    baekjoon | Q14226 (visited 적용)

--- src/main/java/baekjoon/Q14226/Main.java
+++ src/main/java/baekjoon/Q14226/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
     private static int bfs(final int s) {
         final Queue<Command> queue = new LinkedList<>();
+        final boolean[][] visited = new boolean[1000 + 1][1000 + 1]; // [screen][board]
         queue.add(new Command(1, 0, 0));
 
         while(!queue.isEmpty()) {
@@ -23,9 +24,11 @@ public class Main {
                 return command.cnt;
             }
 
-            if(command.board < 0 || command.screen < 0 || command.board >= 10001 || command.screen >= 1001) {
+            if(command.board < 0 || command.screen < 0 || command.board >= visited.length || command.screen >= visited.length || visited[command.screen][command.board]) {
                 continue;
             }
+
+            visited[command.screen][command.board] = true;
 
             queue.add(new Command(command.screen, command.screen, command.cnt + 1));
             queue.add(new Command(command.screen + command.board, command.board, command.cnt + 1));
