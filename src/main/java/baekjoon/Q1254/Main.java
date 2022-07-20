502ee59008941bc2751dd163a8bba409a2044a70
lkhlkh23 <lkhlkh09@gmail.com>
2022-07-20 20:31:39 +0900

    baekjoon | Q1254 (not completed)

---
+++ src/main/java/baekjoon/Q1254/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.Q1254;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // not completed
+
+    public static void main(String[] args) {
+        final char[] inputs = new Scanner(System.in).nextLine().toCharArray();
+        int start = 0;
+        int end = inputs.length - 1;
+        String str = "";
+
+        while(start != end) {
+            if(inputs[start] != inputs[end]) {
+                str = (String.valueOf(inputs[start]) + str);
+                start++;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        for (char input : inputs) {
+            System.out.print(input);
+        }
+        System.out.println(str);
+        // System.out.println(inputs.length + str.length());
+    }
+}
