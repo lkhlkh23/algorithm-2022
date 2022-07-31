d40bbeb26e99920aa28e8b3c4bab613652d281ad
lkhlkh23 <lkhlkh09@gmail.com>
2022-07-31 23:27:51 +0900

    baekjoon | Q2869 (메모리 초과)

--- src/main/java/baekjoon/Q2869/Main.java
+++ src/main/java/baekjoon/Q2869/Main.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
+    // not completed 시간초과
+
     public static void main(String[] args) {
         final String[] inputs = new Scanner(System.in).nextLine().split(" ");
         final int a = Integer.parseInt(inputs[0]);
         final int b = Integer.parseInt(inputs[1]);
         final int v = Integer.parseInt(inputs[2]);
 
-        int current = 0;
-        int day = 0;
+        int start = (v / (a- b)) - 2;
+        int current = start * (a - b);
         while(true) {
             current += a;
-            day++;
+            start++;
             if(current >= v) {
                 break;
             }
             current -= b;
         }
-        System.out.println(day);
+
+        System.out.println(start);
     }
 
 }
