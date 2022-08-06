f5ab6b5eb723c16a966661c46ba287155cdb5556
lkhlkh23 <lkhlkh09@gmail.com>
2022-08-07 00:19:01 +0900

    baekjoon | Q15996

---
+++ src/main/java/baekjoon/Q15596/Test.java
@@ -0,0 +1,14 @@
+package baekjoon.Q15596;
+
+public class Test {
+
+    private static long sum(final int[] a) {
+        long sum = 0;
+        for (final int i : a) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+}
