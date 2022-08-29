ce794c2581d6ebfcb618d077e472c65a165fdacb
lkhlkh23 <lkhlkh09@gmail.com>
2022-08-29 22:24:04 +0900

    baekjoon | Q11660

--- src/main/java/baekjoon/Q11660/Main.java
+++ src/main/java/baekjoon/Q11660/Main.java
@@ -5,30 +5,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        final StringBuilder sb = new StringBuilder();
         final Scanner scanner = new Scanner(System.in);
         final int n = scanner.nextInt();
         final int m = scanner.nextInt();
-        final int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = scanner.nextInt();
+        final int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i][j - 1] + scanner.nextInt();
             }
         }
 
         for (int i = 0; i < m; i++) {
-            int sum = 0;
-            final int startX = scanner.nextInt();
             final int startY = scanner.nextInt();
-            final int endX = scanner.nextInt();
+            final int startX = scanner.nextInt();
             final int endY = scanner.nextInt();
+            final int endX = scanner.nextInt();
 
-            for (int j = startY - 1; j < endY; j++) {
-                for (int k = startX - 1; k < endX; k++) {
-                    sum += arr[j][k];
-                }
+            int sum = 0;
+            for (int j = startY; j <= endY; j++) {
+                sum += ((dp[j][endX] - dp[j][startX - 1]));
             }
-
-            System.out.println(sum);
+            sb.append(sum).append("\n");
         }
+
+        System.out.println(sb.toString());
     }
+
 }
